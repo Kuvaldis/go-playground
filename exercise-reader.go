20f2fe934fa1cd68671f4926ad8eb7ceaf12e1a6
kuvaldis <a1N9u8t9I1k>
2020-01-23 23:47:00 +0300

    reader

---
+++ exercise-reader.go
@@ -0,0 +1,14 @@
+package main
+
+type MyReader struct {
+}
+
+func (r MyReader) Read(b []byte) (int, error) {
+	for i := 0; i < len(b); i++ {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+}
