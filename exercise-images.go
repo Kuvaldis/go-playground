a89f170037bfc625dbc9b6a7ff2258826c24d39a
kuvaldis <a1N9u8t9I1k>
2020-01-29 00:04:46 +0300

    images

---
+++ exercise-images.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+)
+
+type Image struct {
+	width  int
+	height int
+}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rectangle{Min: image.Point{X: 0, Y: 0}, Max: image.Point{X: i.width, Y: i.height}}
+}
+
+func (i Image) At(x, y int) color.Color {
+	v := uint8((x + y) / 2)
+	return color.RGBA{R: v, G: v, B: 255, A: 255}
+}
+
+func main() {
+	//m := Image{width: 200, height: 100}
+	//pic.ShowImage(m)
+}
